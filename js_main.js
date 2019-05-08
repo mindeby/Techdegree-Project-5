156f3f78457c7b6f63dab8f2f45fca8fa61941e1
mindeby <debora.marisa.ribeiro@gmail.com>
2019-05-08 13:08:26 +0200

    div search bar

--- js_main.js
+++ js_main.js
@@ -1,4 +1,3 @@
-
 //Create an array to store the photo paths
 const photoPaths =[];
 
@@ -6,10 +5,10 @@ const photoPaths =[];
 //Create a loop that pushes each photo path into the array we created
 for (var i = 1; i <= 12; i += 1 ) {
   if (i <= 9) {
-      photoPaths.push( "<img src='imgs/photos/0" + i + ".jpg'>" );
+      photoPaths.push( "<img src='imgs/photos/thumbnails/0" + i + ".jpg'>" );
   } else if (i > 9) {
-      photoPaths.push( "<img src='imgs/photos/" + i + ".jpg'>" );
+      photoPaths.push( "<img src='imgs/photos/thumbnails/" + i + ".jpg'>" );
   }
 }
 
-document.write(photoPaths.join(''));
+document.getElementById('photo_container').innerHTML = (photoPaths.join(''));
